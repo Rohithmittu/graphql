d5d6d48845694df70fa028a70467f26c4560f22b
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2025-02-22 00:36:44 +0530

    implemented frontend by using chatgpt just basic implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import TransactionPage from "./pages/TransactionPage.jsx";
+import Header from "./components/ui/Header.jsx";
+import Footer from "./components/ui/Footer.jsx";
+
 function App() {
+  const authUser = true;
+  
   return (
     <>
-      <h1 className="">Hello</h1>
+      {authUser && <Header />}
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+
+      <Footer />
     </>
   );
 }
